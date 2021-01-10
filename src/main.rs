e35fdda9bcbefcab1d5fc4bb9528ffe120c78f93
xffxff <1247714429@qq.com>
2021-01-10 10:12:28 +0800

    custom test framework

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 #![no_std]
 #![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
+mod serial;
 mod vga_buffer;
 
 use core::panic::PanicInfo;
@@ -11,8 +15,44 @@ fn panic(info: &PanicInfo) -> ! {
     loop {}
 }
 
+pub enum QemuExitCode {
+    Success = 0x10,
+    Failed = 0x11,
+}
+
+pub fn exit_qemu(exit_code: QemuExitCode) {
+    use x86_64::instructions::port::Port;
+
+    unsafe {
+        // The 0xf4 is the port where the `isa-debug-exit` device should live.
+        // These are configured in Cargo.toml
+        let mut port = Port::new(0xf4);
+        port.write(exit_code as u32);
+    }
+}
+
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     println!("Hello World{}", "!");
+
+    #[cfg(test)]
+    test_main();
+
     loop {}
+}
+
+#[cfg(test)]
+fn test_runner(tests: &[&dyn Fn()]) {
+    serial_println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
+    exit_qemu(QemuExitCode::Success);
+}
+
+#[test_case]
+fn trivial_assertion() {
+    serial_print!("trivial assertion... ");
+    assert_eq!(0, 1);
+    serial_println!("[ok]");
 }
