bb37477393155c9238d5e99143575f46ab670d3e
xffxff <1247714429@qq.com>
2021-01-10 16:00:01 +0800

    create a library

--- tests/basic_boot.rs
+++ tests/basic_boot.rs
@@ -5,27 +5,14 @@
 #![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
-use simple_os::println;
 
-#[cfg(not(test))]
-#[panic_handler]
-fn panic(info: &PanicInfo) -> ! {
-    println!("{}", info);
-    loop {}
-}
-
-#[no_mangle]
+#[no_mangle] // don't mangle the name of this function
 pub extern "C" fn _start() -> ! {
-    println!("Hello World{}", "!");
-
-    #[cfg(test)]
     test_main();
 
     loop {}
 }
 
-// panic handler in test mode
-#[cfg(test)]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     simple_os::test_panic_handler(info)
