88e96b44eda5785a37c746922b4a0d93bb6c5af6
xffxff <1247714429@qq.com>
2021-01-11 16:36:45 +0800

    double fault handler catch stack overflow

--- src/main.rs
+++ src/main.rs
@@ -21,9 +21,15 @@ pub extern "C" fn _start() -> ! {
     simple_os::init();
 
     // x86_64::instructions::interrupts::int3();
-    unsafe {
-        *(0xdeadbeef as *mut u64) = 42;
+    // unsafe {
+    //     *(0xdeadbeef as *mut u64) = 42;
+    // }
+
+    fn stack_overflow() {
+        stack_overflow();
     }
+
+    stack_overflow();
 
     #[cfg(test)]
     test_main();
