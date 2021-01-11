ec883fb73dbd55c0895681a1c5641958932c1973
xffxff <1247714429@qq.com>
2021-01-11 14:46:11 +0800

    add double fault handler

--- src/main.rs
+++ src/main.rs
@@ -20,7 +20,10 @@ pub extern "C" fn _start() -> ! {
 
     simple_os::init();
 
-    x86_64::instructions::interrupts::int3();
+    // x86_64::instructions::interrupts::int3();
+    unsafe {
+        *(0xdeadbeef as *mut u64) = 42;
+    }
 
     #[cfg(test)]
     test_main();
