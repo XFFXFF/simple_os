e57ce48972426d5a021e173c9a1cc97d270372d5
xffxff <1247714429@qq.com>
2021-01-12 11:30:57 +0800

    keyboard interrupt

--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -163,9 +163,9 @@ fn test_println_output() {
 
     let s = "Some test string that fits on a single line";
     interrupts::without_interrupts(|| {
-        // Since the timer interrupt handler can still run before the test, 
-        // we print an additional newline \n before printing the string s. 
-        // This way, we avoid test failure when the timer handler already 
+        // Since the timer interrupt handler can still run before the test,
+        // we print an additional newline \n before printing the string s.
+        // This way, we avoid test failure when the timer handler already
         // printed some . characters to the current line.
         println!("\n{}", s);
         for (i, c) in s.chars().enumerate() {
