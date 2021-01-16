fea70af713d9ee427ae0308e51ea198c379269bb
xffxff <1247714429@qq.com>
2021-01-16 08:06:54 +0800

    creating a kernel heap

--- src/main.rs
+++ src/main.rs
@@ -8,8 +8,8 @@ extern crate alloc;
 use alloc::boxed::Box;
 use bootloader::{entry_point, BootInfo};
 use core::panic::PanicInfo;
-use simple_os::memory;
 use simple_os::println;
+use simple_os::{allocator, memory};
 use x86_64::{
     structures::paging::{MapperAllSizes, Page, Size4KiB},
     VirtAddr,
@@ -36,27 +36,29 @@ fn kernel_main(boot_info: &'static BootInfo) -> ! {
     let mut frame_allocator =
         unsafe { memory::BootInfoFrameAllocator::init(&boot_info.memory_map) };
 
-    let page = Page::<Size4KiB>::containing_address(VirtAddr::new(0));
-    memory::create_example_mapping(page, &mut mapper, &mut frame_allocator);
-
-    let page_ptr: *mut u64 = page.start_address().as_mut_ptr();
-    unsafe { page_ptr.offset(400).write_volatile(0x_f021_f077_f065_f04e) };
-
-    let addresses = [
-        0xb8000,
-        0x201008,
-        0x0100_0020_1a10,
-        boot_info.physical_memory_offset,
-    ];
+    allocator::init_heap(&mut mapper, &mut frame_allocator).expect("heap initialization failed");
 
     let x = Box::new(41);
 
-    for &address in addresses.iter() {
-        let virt_addr = VirtAddr::new(address);
-        // let phys = memory::translate_addr(virt_addr, phys_mem_offset);
-        let phys = mapper.translate_addr(virt_addr);
-        println!("{:?} -> {:?}", virt_addr, phys);
-    }
+    // let page = Page::<Size4KiB>::containing_address(VirtAddr::new(0));
+    // memory::create_example_mapping(page, &mut mapper, &mut frame_allocator);
+
+    // let page_ptr: *mut u64 = page.start_address().as_mut_ptr();
+    // unsafe { page_ptr.offset(400).write_volatile(0x_f021_f077_f065_f04e) };
+
+    // let addresses = [
+    //     0xb8000,
+    //     0x201008,
+    //     0x0100_0020_1a10,
+    //     boot_info.physical_memory_offset,
+    // ];
+
+    // for &address in addresses.iter() {
+    //     let virt_addr = VirtAddr::new(address);
+    //     // let phys = memory::translate_addr(virt_addr, phys_mem_offset);
+    //     let phys = mapper.translate_addr(virt_addr);
+    //     println!("{:?} -> {:?}", virt_addr, phys);
+    // }
 
     // let ptr = 0xdeadbeaf as *mut u32;
     // unsafe { *ptr = 42; }
